b65ed5581c09ead6d34125dc7169dc515770af44
aluizio neto <aluizioneto03@hotmail.com>
2020-09-05 11:40:58 -0300

    versão 0.0.1

---
+++ src/main/java/com/sirius/api/CursoReferenciaApplication.java
@@ -0,0 +1,13 @@
+package com.sirius.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoReferenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoReferenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sirius/api/CursoReferenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirius.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoReferenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
